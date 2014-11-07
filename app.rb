c50fdd842fc3bbf81ea8c1d7976488e5011c461c
Rock48 <cquiglz@gmail.com>
2014-11-06 22:09:25 -0500

    Allowed user to create blag poss

--- app.rb
+++ app.rb
@@ -2,10 +2,21 @@ require 'sequel'
 require 'sqlite3'
 require 'sinatra'
 
+# Connect to the database sqlite file
 database = Sequel.connect('sqlite://databases/blog.sqlite')
 
-
 get '/' do
-  @posts = database[:posts]
-  erb :blog
+    # Retrieve the table as a ruby object
+    @posts = database[:posts]
+
+    # Run and send the page
+    erb :blog
+end
+
+post '/' do
+    # Retrieve the table as a ruby object
+    @posts = database[:posts]
+
+    # Insert the user's data into the database.
+    @posts.insert(:title => params[:title], :body => params[:content], :creator => params[:creator], :time => Time.now.to_i)
 end
